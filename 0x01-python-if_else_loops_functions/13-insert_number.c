c09f5c72a6c870d7aa55034e226151600bbdaf7d
EshietIy <eshietinyang@gmail.com>
2021-09-07 21:56:56 +0100

    create a node

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,33 +1,37 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * insert_node -  inserts a number into a sorted singly linked list
- * @head: singly list head
- * @number: number to insert
- * Return: the address of the new node, or NULL if it failed
+ * insert_node - inserts a number into a sorted singly linked list.
+ * @head: list head
+ * @number: number to store in the new node
+ * Return: pointer to the new node
  */
 listint_t *insert_node(listint_t **head, int number)
 {
+listint_t *runner;
 listint_t *new;
-listint_t *current;
-current = *head;
+runner = *head;
 new = malloc(sizeof(listint_t));
 if (new == NULL)
 return (NULL);
 new->n = number;
-if (*head == NULL)
+if (*head == NULL || (*head)->n > number)
+{
+new->next = *head;
 *head = new;
-else
-{
-while (current->next != NULL)
-{
-if (current->next->n > number)
-break;
-current = current->next;
+return(new);
 }
-new->next = current->next;
-current->next = new;
+while(runner->next != NULL)
+{
+if ((runner->next)->n >= number)
+{
+new->next = runner->next;
+runner->next = new;
+return(new);
 }
-return (new);
+runner = runner->next;
+}
+new->next = NULL;
+runner->next = new;
+return(new);
 }
