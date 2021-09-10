f2ff0e0ef0fe8fc9051ad6fe093b5adc6d025dd4
EshietIy <eshietinyang@gmail.com>
2021-09-10 01:19:16 +0100

    fixed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -18,4 +18,3 @@ if __name__ == '__main__':
     else:
         print('Usage: {:s} <a> <operator> <b>'.format(sys.argv[0]))
         sys.exit(1)
-                                                            
