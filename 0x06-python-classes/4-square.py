3ff959c33fc4c3d4a396d0e09d3f831ffa2565c4
EshietIy <eshietinyang@gmail.com>
2021-09-29 22:08:50 +0100

    fixed bug

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,31 +13,31 @@ class Square:
         """
         self.size = size
 
-        @property
-        def size(self):
-            """Retrieves the size of this Square.
-            Returns:
-            int: The size of this Square.
-            """
-            return self.__size
+    @property
+    def size(self):
+        """Retrieves the size of this Square.
+        Returns:
+        int: The size of this Square.
+        """
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            """Updates the size of this Square.
-            Args:
-            value (int): The new size of this Square.
-            """
-            if not isinstance(value, int):
-                raise TypeError('size must be an integer')
+    @size.setter
+    def size(self, value):
+        """Updates the size of this Square.
+        Args:
+        value (int): The new size of this Square.
+        """
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        else:
+            if value < 0:
+                raise ValueError('size must be >= 0')
             else:
-                if value < 0:
-                    raise ValueError('size must be >= 0')
-                else:
-                    self.__size = value
+                self.__size = value
 
-        def area(self):
-            """Computes the area of this Square.
-            Returns:
-            int: The area of this Square.
-            """
-            return self.size ** 2
+    def area(self):
+        """Computes the area of this Square.
+        Returns:
+        int: The area of this Square.
+        """
+        return self.size ** 2
