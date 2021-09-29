6f894eadfa4d5dde20d47f51d68b26dfeb87a189
EshietIy <eshietinyang@gmail.com>
2021-09-29 22:14:12 +0100

    a class Square that defines a square by: (based on 2-square.py)

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-'''A module containing the Python class definition of a bytecode.'''
-import math
+"""A module for working with squares.
+"""
 
 
-class MagicClass:
-    '''Represents an object for working with circles.'''
-    def __init__(self, radius=0):
-        '''Initializes this magic class.'''
-        self.__radius = 0
-        if type(radius) is not int and type(radius) is not float:
-            raise TypeError('radius must be a number')
-        self.__radius = radius
+class Square:
+    """Represents a 2D Polygon with 4 equal and perpendicular sides.
+    """
+    def __init__(self, size=0):
+        """Initializes a Square with a given size.
+        Args:
+            size (int): The size of the square.
+        """
+        super().__init__()
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        else:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
 
-        def area(self):
-            '''Computes the area of this circle.'''
-            return self.__radius ** 2 * math.pi
-
-        def circumference(self):
-            '''Computes the circumference of this circle.'''
-            return 2 * math.pi * self.__radius
+    def area(self):
+        """Computes the area of this Square.
+        Returns:
+            int: The area of the Square.
+        """
+        return self.__size ** 2
