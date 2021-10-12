9fdb58a76f4bffab0df2f99139938d38d27072c8
EshietIy <eshietinyang@gmail.com>
2021-10-12 21:40:04 +0100

    a function that reads a text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''a module that define a function that 
+prints to stdout
+'''
+
+def read_file(filename=""):
+    '''a function that prints text file to stdout
+    Args:
+        filename: (str) name or path to file
+    Return:
+        None
+    '''
+    f_list = []
+    with open(filename) as f:
+        f_list = f.readlines()
+        for line in f_list:
+            print(line)
+
+        
