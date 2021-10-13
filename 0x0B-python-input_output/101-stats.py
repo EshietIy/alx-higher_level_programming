39a8e115bced0da45fd2dd7554fb1f515003719f
EshietIy <eshietinyang@gmail.com>
2021-10-13 02:12:14 +0100

    fixed

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-'''task 14 module'''
+'''A script for parsing HTTP logs.
+'''
+import re
 
 
-from sys import stdin
-
-
-status_codes = {
+status_codes_stats = {
     '200': 0,
     '301': 0,
     '400': 0,
@@ -14,31 +13,74 @@ status_codes = {
     '404': 0,
     '405': 0,
     '500': 0
-    }
+}
+'''Stats for the supported status codes.
+'''
+total_file_size = 0
+'''The cummulative sum of the file sizes in each HTTP log.
+'''
+fp = (
+    r'(?P<ip>\d+\.\d+\.\d+\.\d+)',
+    r'(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)',
+    r'(?P<request>[^"]*)',
+    r'(?P<status_code>\d+)',
+    r'(?P<file_size>\d+)'
+)
+'''The pattern for each field in the log.
+'''
+log_fmt = f'\\s*{fp[0]} \\- \\[{fp[1]}\\] "{fp[2]}" {fp[3]} {fp[4]}\\s*'
+'''The regex pattern for the log.
+'''
 
-total_size = i = 0
+
+def print_statistics():
+    '''Prints the accumulated statistics of the HTTP log.
+    '''
+    global total_file_size, status_codes_stats
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
 
 
-def printer():
-    '''this function prints the statistics'''
-    print(f'File size: {total_size}')
-    for key, value in sorted(status_codes.items()):
-        if value > 0:
-            print('{:s}: {:d}'.format(key, value))
+def get_metrics(line):
+    '''Retrieves the metrics from a given HTTP log.
+    Args:
+        line (str): The line of input from which to retrieve the metrics.
+    '''
+    global total_file_size, log_fmt, status_codes_stats
+    trimmed_line = line.rstrip().lstrip()
+    trimmed_line_parts = trimmed_line.split()
+    resp_match = re.fullmatch(log_fmt, line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        total_file_size += file_size
+        if status_code in status_codes_stats.keys():
+            status_codes_stats[status_code] += 1
+    elif len(trimmed_line_parts) >= 2:
+        status_code = trimmed_line_parts[-2]
+        file_size = int(trimmed_line_parts[-1])
+        total_file_size += file_size
+        if status_code in status_codes_stats.keys():
+            status_codes_stats[status_code] += 1
 
 
-try:
-    for line in stdin:
-        splitted_line = line.split()
-    if len(splitted_line) >= 2:
-        status = splitted_line[-2]
-        total_size += int(splitted_line[-1])
-        if status in status_codes:
-            status_codes[status] += 1
-        i += 1
+def run():
+    '''Starts the log parser.
+    '''
+    line_num = 0
+    try:
+        while True:
+            line = input()
+            get_metrics(line)
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics()
+    except (KeyboardInterrupt, EOFError):
+        print_statistics()
 
-        if i % 10 == 0:
-            printer()
-    printer()
-except KeyboardInterrupt as e:
-    printer()
+
+if __name__ == '__main__':
+    run()
