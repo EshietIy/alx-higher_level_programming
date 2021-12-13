9123e3dd8adf81411139480e294e7da932d06983
EshietIy <eshietinyang@gmail.com>
2021-12-13 16:59:28 +0100

    fixed bug

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-let var1 = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-console.log(var1[0] + '\n' + var1[1] + '\n' + var1[2]);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
