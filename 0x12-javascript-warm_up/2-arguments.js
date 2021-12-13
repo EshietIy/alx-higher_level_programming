bf16935af2fdfd466c494a0c63bf35d8f1051317
EshietIy <eshietinyang@gmail.com>
2021-12-13 16:43:42 +0100

    introduction to javascript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let args = process.argv.slice(2);
+if (args.length === 0){
+  console.log('No argument');
+} else {
+  console.log('Arguments found');
+}
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let args = process.argv.slice(2);
+let num = parseInt(args[0]);
+if (num) {
+  console.log('My number: ' + num);
+} else {
+  console.log('Not a number');
+}
