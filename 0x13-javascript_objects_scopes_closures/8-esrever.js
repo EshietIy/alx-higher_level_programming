d3bd09b64bf5d2ede90205678cab2094729b1568
EshietIy <eshietinyang@gmail.com>
2021-12-14 22:11:29 +0100

    bug-fixed

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@
 reverses a list w/o builtin call reverse
 */
 exports.esrever = function (list) {
-  let result = [];
+  const result = [];
 
   for (let i = list.length - 1; i >= 0; i--) {
     result.push(list[i]);
